bcbca6246af8b3f54dd23b99e141dc76afc01c24
giuliana <giuliana.silva@outsystems.com>
2024-09-19 18:59:06 +0100

    Add modal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonModal } from '@ionic/react';
+import React, { useRef } from 'react';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const modal = useRef<HTMLIonModalElement>(null);
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>App</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding">
+        <IonButton id="open-modal" expand="block">
+          Open Sheet Modal
+        </IonButton>
+        <IonModal ref={modal} trigger="open-modal" initialBreakpoint={0.25} breakpoints={[0, 0.25, 0.5, 0.75]} focusTrap={true}>
+          <IonContent className="ion-padding">
+            <div>Test</div>
+            <IonButton>
+              Test button
+            </IonButton>
+            <IonButton>
+              Test button
+            </IonButton>
+          </IonContent>
+        </IonModal>
       </IonContent>
     </IonPage>
   );
